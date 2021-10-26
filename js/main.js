250c0ea0fb652e43ffe619a1033d879c927ae134
Bilal <learn.with.bilal@gmail.com>
2021-10-26 15:13:09 +0300

    add bootstrap

---
+++ js/main.js
@@ -0,0 +1,46 @@
+$('.back').click(function(){
+    window.location.href = '../index.html';
+})
+
+$('#ess_comp').click(function(){
+    window.location.href = 'pages/essential.html';
+
+})
+
+$('#word').click(function(){
+    window.location.href = 'pages/word.html';
+})
+
+$('#excel').click(function(){
+    window.location.href = 'pages/excel.html';
+})
+
+$('#access').click(function(){
+    window.location.href = 'pages/access.html';
+})
+
+$('#access').click(function(){
+    window.location.href = 'pages/powerpoint.html';
+})
+
+$('#outlook').click(function(){
+    window.location.href = 'pages/outlook.html';
+})
+
+$('#it').click(function(){
+    window.location.href = 'pages/outlook.html';
+})
+
+
+$('#attendance').click(function(){
+    window.location.href = 'pages/attendance.html';
+})
+
+$('#points').click(function(){
+    window.location.href = 'pages/points.html';
+})
+
+
+$('#intro').click(function(){
+    window.location.href = 'files/pdf/01.pdf';
+})

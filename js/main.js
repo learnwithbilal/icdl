0a089f6b21a5cf61255367b5ac4dd9b4568851c2
learnwithbilal <learn.with.bilal@gmail.com>
2021-11-26 23:09:37 +0200

    Add Files

--- js/main.js
+++ js/main.js
@@ -11,32 +11,37 @@ $("#word").click(function () {
 });
 
 $("#excel").click(function () {
-  window.location.href = "pages/excel.html";
+  window.location.href =
+    "https://drive.google.com/file/d/1tiGTE5gyx1dEW1zGP37fC8tmCV2ZNG0n/view?usp=sharing";
 });
 
 $("#access").click(function () {
-  window.location.href = "pages/access.html";
+  window.location.href =
+    "https://drive.google.com/file/d/1QaqRm8HDjVtRJ3q7IJpwg99EzNauCZN0/view?usp=sharing";
 });
 
 $("#powerpoint").click(function () {
-  window.location.href = "pages/powerpoint.html";
+  window.location.href =
+    "https://drive.google.com/file/d/1FVCVfEhcnalviCdkZelLYLBoM2sXrf7t/view?usp=sharing";
 });
 
 $("#outlook").click(function () {
-  window.location.href = "pages/outlook.html";
+  window.location.href =
+    "https://drive.google.com/file/d/1NS2ijxeAAVWPth_-xNE1Cj3Xl2Sw9J55/view?usp=sharing";
 });
 
 $("#it").click(function () {
-  window.location.href = "pages/it.html";
+  window.location.href =
+    "https://drive.google.com/file/d/1Jm3zxbya-3BF1y8hneooy4HHLO146m5Y/view?usp=sharing";
 });
 
-$("#attendance").click(function () {
-  window.location.href = "pages/attendance.html";
-});
+// $("#attendance").click(function () {
+//   window.location.href = "pages/attendance.html";
+// });
 
-$("#points").click(function () {
-  window.location.href = "pages/points.html";
-});
+// $("#points").click(function () {
+//   window.location.href = "pages/points.html";
+// });
 
 $("#intro").click(function () {
   window.location.href =
@@ -44,11 +49,13 @@ $("#intro").click(function () {
 });
 
 $("#intro01").click(function () {
-  window.location.href = "../files/pdf/02.pdf";
+  window.location.href =
+    "https://drive.google.com/file/d/1OK3iX0hrFOVmuZMs6Sue8oot5SoS2hSc/view?usp=sharing";
 });
 
 $("#intro02").click(function () {
-  window.location.href = "../files/pdf/03.pdf";
+  window.location.href =
+    "https://drive.google.com/file/d/1CzLOcOP9dWgqudNtiuDRlZzlZlxiIHXH/view?usp=sharing";
 });
 
 $("#ext").click(function () {
@@ -60,5 +67,6 @@ $("#backToExt").click(function () {
 });
 
 $("#mailing").click(function () {
-  window.location.href = "../files/word/example_mailing.pdf";
+  window.location.href =
+    "https://drive.google.com/file/d/1HG8cfz1YYIzPzSnRxmhRZ4l9hFxjw66J/view?usp=sharing";
 });
